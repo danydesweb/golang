a6b6f0b0e156a5c486306196814532164b0b040c
danydesweb <55587083+danydesweb@users.noreply.github.com>
2019-12-03 22:42:02 -0300

    Update broker1.go

--- broker1.go
+++ broker1.go
@@ -8,46 +8,44 @@ import (
   "time"
 )
 
-// define una función para el manejador de mensajes predeterminado
+//define a function for the default message handler
 var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
   fmt.Printf("TOPIC: %s\n", msg.Topic())
   fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
 func main() {
-  //crear una estructura ClientOptions configurando la dirección del agente, cliente
-
+  //create a ClientOptions struct setting the broker address, clientid, turn
+  //off trace output and set the default message handler
   opts := MQTT.NewClientOptions().AddBroker("tcp://mqtt.eclipse.org:1883")
-  opts.SetClientID("go-sample")
+  opts.SetClientID("go-simple1")
   opts.SetDefaultPublishHandler(f)
 
-  //crear e iniciar un cliente usando las ClientOptions 
+  //create and start a client using the above ClientOptions
   c := MQTT.NewClient(opts)
   if token := c.Connect(); token.Wait() && token.Error() != nil {
     panic(token.Error())
   }
 
-  //subscribe al topic /go-mqtt/sample y solicita mensajes
-  // espera el recibo para confirmar la suscripcion
-
-
-  if token := c.Subscribe("go-mqtt/sample ", 0, nil); token.Wait() && token.Error() != nil {
+  //subscribe to the topic /go-mqtt/sample and request messages to be delivered
+  //at a maximum qos of zero, wait for the receipt to confirm the subscription
+  if token := c.Subscribe("go-mqtt/sample1", 0, nil); token.Wait() && token.Error() != nil {
     fmt.Println(token.Error())
     os.Exit(1)
   }
 
-  //Publica  5 mensajes en /go-mqtt/sample 
-  // el servidor después de envia cada mensaje
-  for i := 0; i < 5; i++ {
-    text := fmt.Sprintf("Yo soy Danielito,Fer me tomando sabes cuantas cervezas?????  #%d!", i)
-    token := c.Publish("go-mqtt/sample", 0, false, text)
+  //Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
+  //from the server after sending each message
+  for i := 1; i < 5; i++ {
+    text := fmt.Sprintf("Fer Sabes cuantas cervezas me tomare ahora??? #%d!", i)
+    token := c.Publish("go-mqtt/sample1", 0, false, text)
     token.Wait()
   }
 
   time.Sleep(3 * time.Second)
 
   //unsubscribe from /go-mqtt/sample
-  if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
+  if token := c.Unsubscribe("go-mqtt/sample1"); token.Wait() && token.Error() != nil {
     fmt.Println(token.Error())
     os.Exit(1)
   }
