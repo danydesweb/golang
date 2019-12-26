961ebbce5138544cb675d33adf096f6638aac67f
danydesweb <55587083+danydesweb@users.noreply.github.com>
2019-12-26 17:22:52 -0300

    Update demo.go

--- demo.go
+++ demo.go
@@ -1,17 +1,17 @@
 package main
 
 import(
-	(
+	
 		"fmt"
 		//import the Paho Go MQTT library
 		MQTT "github.com/eclipse/paho.mqtt.golang"
 		"os"
 		"time"
-	   
+	    "encoding/json"
 	  )
-)
 
-type struct DATOS{
+
+type Datos  struct {
 	Grados int
 	Centesimas int
 	nombre string
@@ -25,20 +25,14 @@ var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 	fmt.Printf("TOPIC: %s\n", msg.Topic())
 	fmt.Printf("MSG: %s\n", msg.Payload())
 
-
-
+}
 
 	
 func main() {
 
 		http.HandleFunc("/", )
-		http.HandleFunc("/ambiente", "")
-		http.HandleFunc("/sensores", "")
+		http.HandleFunc("/ambiente", ambientes)
+		http.HandleFunc("/sensores", sensores)
 		http.ListenAndServe(":3005", nil)
 
 	}
-	
-  
-	c.Disconnect(250)
-
-}
