11ba470ac11391ebb4f634e1154b48d66fc3c59c
danydesweb <55587083+danydesweb@users.noreply.github.com>
2019-12-26 16:55:10 -0300

    Update demo.go

--- demo.go
+++ demo.go
@@ -11,64 +11,34 @@ import(
 	  )
 )
 
-type struct temp{
+type struct DATOS{
 	Grados int
 	Centesimas int
-}
-
-type struct Dispositivo{
 	nombre string
 	tipo string
-}
-
-type struct event
-func main(){
 	event string
-}
+	deviceType int
+	eventType int
 
 
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
 
 
 func main{
-	
-	func main{
 
-	//crea a ClientOptions struct configuracion de la direccion broker, clientid, 
-	opts := MQTT.NewClientOptions().AddBroker("tcp://mqtt.eclipse.org:1883")
-	opts.SetClientID("go-simple1")
-	opts.SetDefaultPublishHandler(f)
-  
-	//crea e incia  el cliente usando  ClientOptions
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-	  panic(token.Error())
+	
+	func main() {
+
+		http.HandleFunc("/", )
+		http.HandleFunc("/ambiente", "")
+		http.HandleFunc("/sensores", "")
+		http.ListenAndServe(":3005", nil)
+
 	}
-  
-	//subscribe al topic /go-mqtt/sample and request messages to be delivered
-   
-	if token := c.Subscribe("go-mqtt/sample1", 0, nil); token.Wait() && token.Error() != nil {
-	  fmt.Println(token.Error())
-	  os.Exit(1)
-	}
-  
-	//Publica 5 messages en /go-mqtt/sample 
-  
-	for i := 1; i < 5; i++ {
-	  text := fmt.Sprintf("registro numero  #%d!", i)
-	  token := c.Publish("go-mqtt/sample1", 0, false, text)
-	  token.Wait()
-	}
-  
-	time.Sleep(3 * time.Second)
-  
-	//borra suscripcion para  /go-mqtt/sample
-	if token := c.Unsubscribe("go-mqtt/sample1"); token.Wait() && token.Error() != nil {
-	  fmt.Println(token.Error())
-	  os.Exit(1)
-	}
+	
   
 	c.Disconnect(250)
-
-}
 
 }
