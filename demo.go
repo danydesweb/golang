fa1aab75d9d6b0d5b179a70b2e3a77eda29b9dbe
danydesweb <55587083+danydesweb@users.noreply.github.com>
2019-12-26 16:31:20 -0300

    Add files via upload

---
+++ demo.go
@@ -0,0 +1,34 @@
+package main
+
+import(
+	(
+		"fmt"
+		//import the Paho Go MQTT library
+		MQTT "github.com/eclipse/paho.mqtt.golang"
+		"os"
+		"time"
+	   
+	  )
+)
+
+type struct temp{
+	Grados int
+	Centesimas int
+}
+
+type struct Dispositivo{
+	nombre string
+	tipo string
+}
+
+type struct event
+func main(){
+	event string
+}
+
+
+
+
+func main{
+
+}
