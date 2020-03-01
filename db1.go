1760f90472123effa9f86339f6fe929976de8250
danydesweb <55587083+danydesweb@users.noreply.github.com>
2020-02-29 21:52:46 -0300

    Update db1.go
    credenciales de elephansql
    usuario paezdanypaez@gmail.com
    contraseña 1204dany1976

--- db1.go
+++ db1.go
@@ -1,14 +1,134 @@
-package main()
+package main
 
 import (
+  	"log"
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/postgres"
+    "fmt"
+    "net/http"
+    "strconv"
+    "github.com/labstack/echo"
 )
+                                                                                                                                                                                                                                  
+/*type Users struct{
+	
+  id string
+  name string
+  mail string
+  phone uint
+  
+} 
 
-func main() {
-  db, err := gorm.Open("vtwlajng", "host=
-  	postgres://vbqlsvuc:qKDFKe1BvF1KlT8Itt7S8Xu5bAmoJkZA@rajje.db.elephantsql.com: port=5432 user=vtwlajng dbname=vtwlajng password=U27Dryhunm7bdKbAinQ5KcRXc894A-Nn")
-  defer db.Close()
+type Dispositivos struct{
+	
+  id string
+  protocol string
+  power_source string
+  input_output string
+  voltage uint
+  
+}*/
+
+type User struct {
+	gorm.Model `json:"model"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+}
+
+type dbops struct {
+	db *gorm.DB
+}
+func main() {                                                                                                                                                                                                                        
+  db, err := gorm.Open("postgres", "host=hansken.db.elephantsql.com port=5432 user=vtwlajng dbname=vtwlajng password=U27Dryhunm7bdKbAinQ5KcRXc894A-Nn")                                                                              
+                                                                                                                                                                                                                                     
+  if err != nil {                                                                                                                                                                                                                    
+    log.Print(err)
+    
+    initialMigration(db)
+    handleRequest(db)
+  }                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                     
+  log.Print("Conectado!")                                                                                                                                                                                                            
+                                                                                                                                                                                                                                     
+ // defer db.Close()                                                                                                                                                                                                                   
+}
+
+
+
+func handlerFunc(msg string) func(echo.Context) error {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, msg)
+	}
+}
+
+func allUsers(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		var users []User
+		db.Find(&users)
+		fmt.Println("{}", users)
+
+		return c.JSON(http.StatusOK, users)
+	}
+}
+
+func newUser(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		name := c.Param("name")
+		email := c.Param("email")
+		db.Create(&User{Name: name, Email: email})
+		return c.String(http.StatusOK, name+" user successfully created")
+	}
+}
+
+func deleteUser(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		name := c.Param("name")
+
+		var user User
+		db.Where("name = ?", name).Find(&user)
+		db.Delete(&user)
+
+		return c.String(http.StatusOK, name+" user successfully deleted")
+	}
+}
+
+func updateUser(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		name := c.Param("name")
+		email := c.Param("email")
+		var user User
+		db.Where("name=?", name).Find(&user)
+		user.Email = email
+		db.Save(&user)
+		return c.String(http.StatusOK, name+" user successfully updated")
+	}
+}
+
+func usersByPage(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		limit, _ := strconv.Atoi(c.QueryParam("limit"))
+		page, _ := strconv.Atoi(c.QueryParam("page"))
+		var result []User
+		db.Limit(limit).Offset(limit * (page - 1)).Find(&result)
+		return c.JSON(http.StatusOK, result)
+	}
+}
+
+func handleRequest(db *gorm.DB) {
+	e := echo.New()
+
+	e.GET("/users", allUsers(db))
+	e.GET("/user", usersByPage(db))
+	e.POST("/user/:name/:email", newUser(db))
+	e.DELETE("/user/:name", deleteUser(db))
+	e.PUT("/user/:name/:email", updateUser(db))
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func initialMigration(db *gorm.DB) {
+
+	db.AutoMigrate(&User{})
 }
 
 
